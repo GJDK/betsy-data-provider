2281e1382dbd0ae3a2acaba91af2c16f0ec8dc56
DeneshGJ <102808564+DeneshGJ@users.noreply.github.com>
2025-03-28 20:41:01 +0530

    Pointed to zefire endpoint

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 // Global configuration for Ollama settings
+// const config = {
+//     OLLAMA_URL: 'http://localhost:11434/api/generate', // Change if required
+//     MODEL_NAME: 'qwen2.5:3b' // Change the model name if needed
+//   };
+
 const config = {
-    OLLAMA_URL: 'http://localhost:11434/api/generate', // Change if required
-    MODEL_NAME: 'qwen2.5:3b' // Change the model name if needed
+    OLLAMA_URL: 'http://10.2.76.24:11434/api/generate', // Change if required
+    MODEL_NAME: 'qwen2.5:3b-instruct-q4_K_M' // Change the model name if needed
   };
   
   module.exports = config;
