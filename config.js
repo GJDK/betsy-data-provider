d14b9853df6e62c12253d54c12a037a1c212f02a
DeneshGJ <102808564+DeneshGJ@users.noreply.github.com>
2025-03-29 22:49:31 +0530

    Pointed to zefire cloud endpoint

--- config.js
+++ config.js
@@ -4,10 +4,15 @@
 //     MODEL_NAME: 'qwen2.5:3b' // Change the model name if needed
 //   };
 
+// const config = {
+//     OLLAMA_URL: 'http://10.2.76.24:11434/api/generate', // Change if required
+//     MODEL_NAME: 'qwen2.5:3b-instruct-q4_K_M' // Change the model name if needed
+//   };
+
 const config = {
-    OLLAMA_URL: 'http://10.2.76.24:11434/api/generate', // Change if required
-    MODEL_NAME: 'qwen2.5:3b-instruct-q4_K_M' // Change the model name if needed
-  };
+  OLLAMA_URL: 'https://l3alw4jfcnbz4br7qio6g3oei40lvwyc.lambda-url.us-west-2.on.aws/sysrupt5/zeefireai/api/generate', // Change if required
+  MODEL_NAME: 'qwen2.5:3b' // Change the model name if needed
+};
   
   module.exports = config;
   
