de6adde73cde7b434e655304aeef71f27a1338d3
DeneshGJ <102808564+DeneshGJ@users.noreply.github.com>
2025-04-01 10:55:22 +0530

    Corrected the model name

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 
 const config = {
   OLLAMA_URL: 'https://l3alw4jfcnbz4br7qio6g3oei40lvwyc.lambda-url.us-west-2.on.aws/sysrupt5/zeefireai/api/generate', // Change if required
-  MODEL_NAME: 'qwen2.5:3b' // Change the model name if needed
+  MODEL_NAME: 'qwen2.5:3b-instruct-q4_K_M' // Change the model name if needed
 };
   
   module.exports = config;
